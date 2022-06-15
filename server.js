bb45e5f6f49f74db3a6c99ffa5c59c5b2345ed34
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-15 11:25:09 +0200

    showing employees

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 import express from 'express';
+import axios from 'axios';
 
 const app = express();
 const port = 3009;
 
+const employees = (await axios.get('https://edwardtanguay.netlify.app/share/employees.json')).data;
+
 app.get('/', (req, res) => {
-	res.send(`this is the <b>info site333</b>`)
+	res.send(`
+<h1>Info Site</h1>	
+<h2>There are ${employees.length} employees</h2>	
+<ul>
+	${employees.map(employee => `<li>${employee.lastName}</li>`).join('')}
+</ul>
+	`)
 });
 
 app.listen(port, () => {

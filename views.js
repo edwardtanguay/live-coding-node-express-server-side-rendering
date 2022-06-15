ff968659d7a14e050bdfa6739a1c88cb358321fc
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-15 12:12:33 +0200

    link fixed

--- views.js
+++ views.js
@@ -6,7 +6,7 @@ export const siteView = ({employees}) => {
 	<meta charset="UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<link rel="stylesheet" type="text/css" href="main.css" />	
+	<link rel="stylesheet" type="text/css" href="main.css" />	
 	<title>Info Site</title>
 </head>
 <body>
